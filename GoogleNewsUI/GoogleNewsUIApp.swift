80152abf63760fc58ef640d365375ee3821e42ad
David Leyva <dleyvaabrahantes@yahoo.com>
2022-03-15 17:19:29 -0400

    Initial Commit

---
+++ GoogleNewsUI/GoogleNewsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleNewsUIApp.swift
+//  GoogleNewsUI
+//
+//  Created by David on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GoogleNewsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
